c37dfb25405905e3a366e8fca7e4771dd5ed4c53
ErmiasYT <88579472+ErmiasYT@users.noreply.github.com>
2025-06-20 14:12:36 -0700

    Update summary.py

--- app/api/summary.py
+++ app/api/summary.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
-from db.supabase import get_supabase_client
-from auth.supabase_jwt import get_current_user_id
-from models.schemas import UserSummary
+from app.db.supabase import get_supabase_client
+from app.auth.supabase_jwt import get_current_user_id
+from app.models.schemas import UserSummary
 from datetime import datetime, timedelta
 
 router = APIRouter()
