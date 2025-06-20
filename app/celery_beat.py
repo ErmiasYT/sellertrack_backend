8a89175d5a9b710a7387ba90cabc813c10ec3d6a
ErmiasYT <88579472+ErmiasYT@users.noreply.github.com>
2025-06-20 12:43:47 -0700

    Create celery_beat.py

---
+++ app/celery_beat.py
@@ -0,0 +1,18 @@
+from celery import Celery
+from celery.schedules import crontab
+from app.config import settings
+
+celery_beat = Celery(
+    "beat",
+    broker=settings.REDIS_URL,
+    backend=settings.REDIS_URL
+)
+
+celery_beat.conf.beat_schedule = {
+    "run-due-queue-every-minute": {
+        "task": "app.workers.queue_runner.run_due_queue",
+        "schedule": crontab(minute="*"),
+    },
+}
+
+celery_beat.conf.timezone = "UTC"
