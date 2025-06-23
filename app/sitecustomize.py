837af548696daf07a27a36672d41d996c5637a20
ErmiasYT <88579472+ErmiasYT@users.noreply.github.com>
2025-06-23 15:05:42 -0700

    Update and rename celery_patch.py to sitecustomize.py

--- app/sitecustomize.py
+++ app/sitecustomize.py
@@ -1,15 +1,11 @@
 import os
 from kombu.transport.redis import Channel
 
-BRPOP_TIMEOUT = 60 #sec
-    
-# make sure we only patch once
+BRPOP_TIMEOUT = int(os.getenv("CELERY_BRPOP_TIMEOUT", "60"))
+
 if not getattr(Channel, "_long_poll_patched", False):
-
     def drain_events_long_poll(self, connection, timeout=None):
-        # Ignore Celery’s 1-second timeout and always block BRPOP_TIMEOUT
         return self._brpop(connection, timeout=BRPOP_TIMEOUT)
-
     Channel.drain_events = drain_events_long_poll
     Channel._long_poll_patched = True
     print(f"[✅ PATCH LOADED] Redis BRPOP timeout → {BRPOP_TIMEOUT}s")
